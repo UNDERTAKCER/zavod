e3f314d722b25d9bc74c67e4a25f541067cee42f
underaker0 <pitirmitrich02@gmail.com>
2024-05-30 14:20:22 +0400

    gg

--- zavod/zavod/adminPanel/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, '../templates/start/index.html')
-
-
-def choose(request):
-    return render(request, '../adminPanel/templates/start/choose.html')
